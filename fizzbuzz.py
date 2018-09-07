fc32c40baa0d6be3785d1b17cec7ad46fa154f1c
josh nickson <jnickson1506@gmail.com>
2018-09-07 11:28:59 +0100

    return buzz for multiples of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,8 @@
 def fizzbuzz(x):
     if x % 3 == 0:
         return 'Fizz'
+    elif x % 5 == 0:
+        return 'Buzz'
     else:
         return x
+
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -10,3 +10,8 @@ def test_fizzbuzz_multiples_of_3():
     assert fizzbuzz(3) == 'Fizz'
     assert fizzbuzz(24) == 'Fizz'
     assert fizzbuzz(57) == 'Fizz'    
+
+def test_fizzbuzz_multiples_of_5():
+    assert fizzbuzz(5) == 'Buzz'
+    assert fizzbuzz(10) == 'Buzz'
+    assert fizzbuzz(50) == 'Buzz'  
